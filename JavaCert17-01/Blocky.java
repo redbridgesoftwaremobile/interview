b105190f2bcce46a3c09d28fdc46c5f1f4b61fb7
redbridgesoftwaremobile <redbridgesoftwaremobile@gmail.com>
2023-11-25 12:39:10 +0000

    nov 25 certguide

---
+++ JavaCert17-01/Blocky.java
@@ -0,0 +1,19 @@
+
+public class Blocky{
+
+
+      public static void main(String[] args){
+          String valor  = "5";
+          String blocky = """
+                          \srenglon1 \
+                          renglon2 + valor;
+                          """;
+
+          System.out.println(blocky);
+
+      }
+
+
+
+}
+
